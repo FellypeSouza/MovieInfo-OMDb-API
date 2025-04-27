39d7ce2664c9b9718c74dbca91fc4e5a137de520
FellypeSouza <fellypedev23@gmail.com>
2025-04-27 20:30:12 -0300

    estilizando o conteudo central

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from '../components/nav/nav.component';
+import { ContentComponent } from '../components/content/content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
