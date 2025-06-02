7747f9c859305fdfb466c29c57095197e04acc8f
Fellype Souza <fellypedev23@gmail.com>
2025-06-01 22:59:21 -0300

    terminando a adição de responsividade as páginas e finalizando o projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavComponent } from '../components/nav/nav.component';
-import { ContentComponent } from '../components/content/content.component';
 import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, ContentComponent, FormsModule],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
